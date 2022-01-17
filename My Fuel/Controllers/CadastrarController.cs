4601fd93e51f43b1cafc356c946236fe1631e62d
frateu <matheus.fanjos@outlook.com>
2022-01-17 16:27:17 -0300

    Fuel supply page created

--- My Fuel/Controllers/CadastrarController.cs
+++ My Fuel/Controllers/CadastrarController.cs
@@ -18,19 +18,34 @@ namespace My_Fuel.Controllers
         {
             CultureInfo nonInvariantCulture = new CultureInfo("en-US");
             Thread.CurrentThread.CurrentCulture = nonInvariantCulture;
-            int alcoolBit = 0;
+            int alcoolInt = 0;
+            string alcoolStr = "Gasolina";
+
             if (abastecimento.Alcool)
             {
-                alcoolBit = 1;
+                alcoolInt = 1;
+                alcoolStr = "Álcool";
             }
-
             string sqlFormattedDate = abastecimento.Data.ToString("yyyy-MM-dd");
 
-            sqlAbastecimento = db.commandTxt("INSERT INTO Abastecimentos (data, km_rodado, km_total, litros, valor, alcool) " +
+            abastecimento.Media = double.Parse(abastecimento.KmRodado, CultureInfo.InvariantCulture) / double.Parse(abastecimento.Litros, CultureInfo.InvariantCulture);
+            abastecimento.ValorLitro = double.Parse(abastecimento.Valor, CultureInfo.InvariantCulture) / double.Parse(abastecimento.Litros, CultureInfo.InvariantCulture);
+
+            sqlAbastecimento = db.commandTxt("INSERT INTO Abastecimentos (data, km_rodado, km_total, litros, valor, alcool, media, valor_litro) " +
                 "VALUES ('" + sqlFormattedDate + "', " + double.Parse(abastecimento.KmRodado, CultureInfo.InvariantCulture) +
                 ", " + double.Parse(abastecimento.KmTotal, CultureInfo.InvariantCulture) + ", " + double.Parse(abastecimento.Litros, CultureInfo.InvariantCulture) +
-                ", " + double.Parse(abastecimento.Valor, CultureInfo.InvariantCulture) + ", " + alcoolBit + ")");
-            return Redirect("/Cadastrar");
+                ", " + double.Parse(abastecimento.Valor, CultureInfo.InvariantCulture) + ", " + alcoolInt + ", " + abastecimento.Media + ", " + abastecimento.ValorLitro + ")");
+
+            ViewBag.KmRodado = double.Parse(abastecimento.KmRodado, CultureInfo.InvariantCulture).ToString("0.##");
+            ViewBag.KmTotal = double.Parse(abastecimento.KmTotal, CultureInfo.InvariantCulture);
+            ViewBag.Litros = double.Parse(abastecimento.Litros, CultureInfo.InvariantCulture);
+            ViewBag.Valor = double.Parse(abastecimento.Valor, CultureInfo.InvariantCulture).ToString("0.##");
+            ViewBag.Data = abastecimento.Data;
+            ViewBag.Alcool = alcoolStr;
+            ViewBag.Media = abastecimento.Media.ToString("0.##");
+            ViewBag.ValorLitro = abastecimento.ValorLitro.ToString("0.##");
+
+            return View();
         }
     }
 }
--- My Fuel/Models/AbastecimentoModel.cs
+++ My Fuel/Models/AbastecimentoModel.cs
@@ -8,5 +8,7 @@
         public string Valor { get; set; }
         public DateTime Data { get; set; }
         public Boolean Alcool { get; set; }
+        public double Media { get; set; }
+        public double ValorLitro { get; set; }
     }
 }
